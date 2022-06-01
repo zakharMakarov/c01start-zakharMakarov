b7314171f0e7d87d44d03720bb5d1366288667a1
zakharMakarov <makarovzakhar1980@gmail.com>
2022-06-01 21:33:15 +0900

    1

---
+++ runner.sh
@@ -0,0 +1,2 @@
+make task1
+./task1
---
+++ task1.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+int main() {
+    float A;
+    std::cin >> A;
+    float rad = A/180 *3.14152926;
+    std::cout << rad << "\n";
+    return 0;
+}
